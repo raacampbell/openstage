1b11c0dd2154a5df9f0721117a3844c3e07d6738
Rob Campbell <git@raacampbell.com>
2015-06-05 09:59:05 +0200

    Update URL for USB host shield in docs.

--- OpenStage/a_includes.ino
+++ OpenStage/a_includes.ino
@@ -3,7 +3,8 @@
 // * Load libraries
 //
 // Load the libraries necessary to interface with the DualShock3 via the USB Host Shield.
-#include <PS3USB.h>
-#include <AccelStepper.h>  //Control of stepper motors
+// Can comment out the PS3USB if you also disable the PS3 gamepad in c_userSettings
+#include <PS3USB.h> //see https://github.com/felis/USB_Host_Shield_2.0
+#include <AccelStepper.h>  //Control of stepper motors: http://www.airspayce.com/mikem/arduino/AccelStepper/
 #include <LiquidCrystal.h> //Write to LCD display (https://bitbucket.org/fmalpartida/new-liquidcrystal/wiki/Home)
 #include <Wire.h> //Seems to be needed on some systems
