566c769609eb38edd11d7c36db2379d4f1569d0a
Rob Campbell <git@raacampbell.com>
2015-06-05 10:14:04 +0200

    Bug fix: with newer versions of the USB Host Shield we can't build unless SPI.h is explicitly included before PS3USB.h is included.

--- OpenStage/a_includes.ino
+++ OpenStage/a_includes.ino
@@ -4,6 +4,7 @@
 //
 // Load the libraries necessary to interface with the DualShock3 via the USB Host Shield.
 // Can comment out the PS3USB if you also disable the PS3 gamepad in c_userSettings
+#include <SPI.h>
 #include <PS3USB.h> //see https://github.com/felis/USB_Host_Shield_2.0
 #include <AccelStepper.h>  //Control of stepper motors: http://www.airspayce.com/mikem/arduino/AccelStepper/
 #include <LiquidCrystal.h> //Write to LCD display (https://bitbucket.org/fmalpartida/new-liquidcrystal/wiki/Home)
